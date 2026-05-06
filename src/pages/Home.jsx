f421bdef2416de6e649e9f70d79d2c7f5b113ee2
Chandreshhere <whoischandresh@gmail.com>
2026-05-06 11:13:57 +0530

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
       <Reveal />
       <Tagline />
       <WhatWeDo />
-      {/* <Services /> */}
+      <Services />
       <AdsPartners />
       <Testimonials />
       <Clients />
